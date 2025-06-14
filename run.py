99163d6f55ce0659ab3856e82b71b5e5922b94e4
yangyanglinb <liyangyangnb@gmail.com>
2025-06-14 13:53:23 +0700

    Update run.py

--- run.py
+++ run.py
@@ -199,5 +199,5 @@ if __name__ == '__main__':
 
         exp = Exp(args)  # set experiments
         print('>>>>>>>testing : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
-        exp.test(setting, test=1)
+        exp.test(setting)
         torch.cuda.empty_cache()
